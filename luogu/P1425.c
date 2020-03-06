9e871588dcd08a74192cba9aff31c7976ee49b7e
ApassEr <31186725+zerowong@users.noreply.github.com>
2020-03-06 18:43:49 +0800

    Delete P1425.c

--- luogu/P1425.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(void){
-    int a, b, c, d, e, f;
-    scanf("%d%d%d%d", &a, &b, &c, &d);
-    printf("%d %d\n",((c * 60 + d) - (a * 60 + b)) / 60, ((c * 60 + d) - (a * 60 + b)) % 60);
-    return 0;
-}
