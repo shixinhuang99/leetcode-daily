b054c14cf2e730ec37ed01ef70a9df487374f4eb
zerowong <wongzero@foxmail.com>
2021-11-02 22:49:29 +0800

    add ts-playground

---
+++ playground/ts/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+
+run="npm run test"
+
+$run
---
+++ playground/ts/test.ts
@@ -0,0 +1,10 @@
+import fs from 'fs'
+import sol from './sol'
+
+const fileContent = fs.readFileSync('./test.json', { encoding: 'utf-8' })
+const data: any[] = JSON.parse(fileContent)
+
+for (const i of data) {
+  const res = sol(i)
+  console.log(res)
+}
