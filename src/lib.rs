4e080b9893e647d36841d857f9810e08fe7ce729
ShiXin Huang <shixinhuang99@gmail.com>
2024-05-08 03:27:35 +0800

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
+#[cfg(test)]
+mod utils;
+
 pub struct Solution;
 
+// start
 
+// end
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use test_case::test_case;
+    use utils::*;
 
     fn test_solution() {
         //
