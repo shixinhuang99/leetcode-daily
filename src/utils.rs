05dc1ca3497d786b738ad3a829ea6ad3f6d7a233
sxh99 <wongzero@foxmail.com>
2024-08-27 17:47:06 +0800

    1185

--- src/utils.rs
+++ src/utils.rs
@@ -1,23 +1,23 @@
 pub fn slice_eq_no_order<T>(v1: &mut [T], v2: &mut [T]) -> bool
 where
-    T: std::cmp::Ord,
+	T: std::cmp::Ord,
 {
-    v1.sort();
-    v2.sort();
+	v1.sort();
+	v2.sort();
 
-    v1 == v2
+	v1 == v2
 }
 
 pub fn str_slice_to_string_vec(ss: &[&str]) -> Vec<String> {
-    ss.iter().map(|s| s.to_string()).collect()
+	ss.iter().map(|s| s.to_string()).collect()
 }
 
 #[cfg(all(feature = "test_utils", test))]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_slice_eq_no_order() {
-        assert!(slice_eq_no_order(&mut [1, 0], &mut [0, 1]));
-    }
+	#[test]
+	fn test_slice_eq_no_order() {
+		assert!(slice_eq_no_order(&mut [1, 0], &mut [0, 1]));
+	}
 }
